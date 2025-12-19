df0e9646662fdbe46f2166452f2425c5d3a00807
Akshay Kumar <devops@learn.com>
2025-12-20 00:44:26 +0530

    V5

--- greeting.py
+++ greeting.py
@@ -1,3 +1,3 @@
 print("Hi I am tesing integration with jenkins")
 print("Hello, welcome to my first Jenkins integration test!")
-print("This is a simple Python script to demonstrate Jenkins integration.")
+print("This is a simple Python script to demonstrate Jenkins integration.\n")
