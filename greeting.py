65d5623763d7c44cb13aa1a71b88fcd2f4b60424
Akshay Kumar <devops@learn.com>
2025-12-20 00:06:15 +0530

    Merge remote main with local main

--- greeting.py
+++ greeting.py
@@ -1,2 +1,3 @@
 print("Hi I am tesing integration with jenkins")
 print("Hello, welcome to my first Jenkins integration test!")
+print("This is a simple Python script to demonstrate Jenkins integration.")
